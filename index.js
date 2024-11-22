f47af9399b22b0f7fa86e6b6a2ae56befcb7a3c0
Lakshan Tharaka <tharakahitibandara@gmail.com>
2024-11-22 13:19:37 +0530

    Remove Datadog tracer initialization from index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // const tracer = require('dd-trace').init()
-import tracer from "dd-trace";  
-tracer.init({
-  service: "auth-service",
-  logInjection: true,
-});
+// import tracer from "dd-trace";  
+// tracer.init({
+//   service: "auth-service",
+//   logInjection: true,
+// });
 
 import express from "express";
 import dotenv from "dotenv";
